fab0b0aef47e1321e08b2c5f2f69c45fc45cdc70
MS041971 <MS041971@CERN-MS041971>
2016-11-21 15:58:32 +0400

    Add project files.

---
+++ UpdateRoute/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.Win32
+Imports System.ServiceProcess
+Imports System.Management
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ''' Enable IP Routing in Registry by updateing the proper key.
+        Dim key As RegistryKey = My.Computer.Registry.LocalMachine
+        Dim subkey As RegistryKey
+        subkey = key.OpenSubKey("SYSTEM\CurrentControlSet\services\Tcpip\Parameters", True)
+        subkey.SetValue("IPEnableRouter", 1)
+
+        '' Verify the Service is enabled (Automatic) and started
+        Dim obj As ManagementObject
+        Dim inParams, outParams As ManagementBaseObject
+        Dim Result As Integer
+        Dim sc As ServiceController
+
+        obj = New ManagementObject("\\.\root\cimv2:Win32_Service.Name='RemoteAccess'")
+        sc = New ServiceController("RemoteAccess")
+
+        'Change the Start Mode to Automatic         
+        If obj("StartMode").ToString = "Disabled" Then
+            'Get an input parameters object for this method  
+            inParams = obj.GetMethodParameters("ChangeStartMode")
+            inParams("StartMode") = "Automatic"
+
+            'do it!               
+            outParams = obj.InvokeMethod("ChangeStartMode", inParams, Nothing)
+            Result = Convert.ToInt32(outParams("returnValue"))
+            sc.Start()
+
+            If Result <> 0 Then
+                Throw New Exception("ChangeStartMode method error code " & Result)
+            End If
+        End If
+
+        '' Push persistent static route to routing table
+        Dim OpenCMD
+        OpenCMD = CreateObject("wscript.shell")
+        OpenCMD.run("cmd.exe /C route add -p 10.0.10.0 mask 255.255.255.0 10.181.209.244")
+
+    End Sub
+
+
+End Class
+
+
+
+
---
+++ UpdateRoute/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("UpdateRoute")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("UpdateRoute")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c42a060d-1153-4871-9311-ca500c8af5c5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
